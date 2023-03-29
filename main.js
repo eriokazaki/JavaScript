e469b552222961bddb71d82d53c104f2b44f3c26
eriokazaki <127730893+eriokazaki@users.noreply.github.com>
2023-03-30 08:33:34 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,96 @@
 let buttons = document.getElementsByClassName(".Button");
-let cal = document.getElementsByClassName(".cal");
 let result =document.getElementById("result");
+let operate = 　document.getElementsByClassName("operator");
 
 function clickButton(target) {
   let target_value = target.innerHTML;
-
+  
   if (target_value == "AC") {
     result.innerHTML = "0";
   } else if (target_value == "=") {
     result.innerHTML = eval(result.innerHTML)　
-  }  else {
+  } else if (target_value == "." && result.innerHTML == "0") {
+    result.innerHTML = "0.";
+  } else if (target_value == "00" && result.innerHTML == "0") {
+    result.innerHTML = "0";
+  } else if (target_value == "*" && result.innerHTML == "0") {
+    result.innerHTML = "0*";
+  } else if (target_value == "/" && result.innerHTML == "0") {
+    result.innerHTML = "0/";
+  } else if (target_value == "+" && result.innerHTML == "0") {
+    result.innerHTML = "0+";
+  } else if (target_value == "-" && result.innerHTML == "0") {
+    result.innerHTML = "0-";
+  } else {
     if (result.innerHTML == "0") {
       result.innerHTML = target_value;
     } else {
       result.innerHTML += target_value;
     }
   }
-
+}
+function operating(target) {
+  let target_value = target.innerHTML;
+  let operate = 　document.getElementsByClassName("operator");
+   
+  if (result.innerHTML.slice(-1) == operate.textContent && target_value == operate.textContent) {
+     const f = result.innerHTML.slice(0,-1)
+     result.innerHTML = f + target_value;
+  }
 }
 
- $('.cal').click(function () { 
- if (result.textContent.slice(-1) === cal.innerHTML) {
-  const f = result.textContent.slice(0,-1)
-  result.textContent = f + cal.textContent;
- }else{
-  result.textContent += cal.textContent;
- }
- }
-})
+function clickplus(target) {
+   let target_value = target.innerHTML;
+   
+   if(result.innerHTML.slice(-1) == "+" && target_value == "+") {
+     let str = result.innerHTML;
+     let newStr = str.slice(0, -1);
+     console.log(newStr);
+     result.innerHTML = newStr + target_value;
+     console.log(result.innerHTML);
+     result.innerHTML = result.innerHTML.slice(0, -1);
+     console.log(result.innerHTML);
+   } 
+}
 
+function clickminus(target) {
+  let target_value = target.innerHTML;
+  
+  if(result.innerHTML.slice(-1) == "-" && target_value == "-") {
+    let str = result.innerHTML;
+    let newStr = str.slice(0, -1);
+    console.log(newStr);
+    result.innerHTML = newStr + target_value;
+    console.log(result.innerHTML);
+    result.innerHTML = result.innerHTML.slice(0, -1);
+    console.log(result.innerHTML);
+  }
+}
+
+function clickmulti(target) {
+  let target_value = target.innerHTML;
+  
+  if(result.innerHTML.slice(-1) == "*" && target_value == "*") {
+    let str = result.innerHTML;
+    let newStr = str.slice(0, -1);
+    console.log(newStr);
+    result.innerHTML = newStr + target_value;
+    console.log(result.innerHTML);
+    result.innerHTML = result.innerHTML.slice(0, -1);
+    console.log(result.innerHTML);
+  }
+}
+
+function clickdivi(target) {
+  let target_value = target.innerHTML;
+  
+  if(result.innerHTML.slice(-1) == "/" && target_value == "/") {
+    let str = result.innerHTML;
+    let newStr = str.slice(0, -1);
+    console.log(newStr);
+    result.innerHTML = newStr + target_value;
+    console.log(result.innerHTML);
+    result.innerHTML = result.innerHTML.slice(0, -1);
+    console.log(result.innerHTML);
+  }
+}
